496b5e45d2ae94b619bd13522a13662984ce535f
Rahul Bhowmick <rahul.bhowmick@okta.com>
2025-11-27 15:20:55 +0530

    test(react): added integration test cases for domain management component (#198)
    * test: added coverage for domain verify & delete modal
    
    * test: add domain management component test coverage
    
    * test: add domain management component test coverage no trans
    
    * chore: self review, removed unused code and fixed mock types
    
    * Fix/domain management (#200)
    
    * fix(core): make i18 messages optinal
    
    * fix(core): make i18 messages optinal
    
    * chore: review comments addressed
    
    * test: corrected test case wrt to loader changes
    
    ---------
    
    Co-authored-by: Naveen Chand <naveen.s.chand@okta.com>

--- packages/react/src/internals/__mocks__/index.ts
+++ packages/react/src/internals/__mocks__/index.ts
@@ -3,3 +3,4 @@ export * from './core/core-client.mocks';
 export * from './core/i18n-service.mocks';
 
 export * from './my-org/org-management/org-details.mocks';
+export * from './my-org/domain-management/domain.mocks';
